16080618575b85e2976fceb4f4bd53badc7e7ad2
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2020-10-22 21:43:24 +0530

    Updates

---
+++ precache-manifest.8415a6c46690a9fd483a4597282dc1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659cb5c9b520a0ca709a4651bcfb6e5b",
+    "url": "/React-ToDolist/index.html"
+  },
+  {
+    "revision": "6665373ec467ebeecc8f",
+    "url": "/React-ToDolist/static/css/main.9215417e.chunk.css"
+  },
+  {
+    "revision": "b9919180e080dd306c80",
+    "url": "/React-ToDolist/static/js/2.2ed79bbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDolist/static/js/2.2ed79bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6665373ec467ebeecc8f",
+    "url": "/React-ToDolist/static/js/main.c03c9759.chunk.js"
+  },
+  {
+    "revision": "c1abef7252e780af7525",
+    "url": "/React-ToDolist/static/js/runtime-main.ad8a4966.js"
+  }
+]);
